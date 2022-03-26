3a6b11b04ed4fb6e8a451a78affed92279e8eab0
AliHossain01 <alihossaincse01@gmail.com>
2022-03-26 20:57:10 +0600

    final

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Item.css'
 const Fish = ({ fish }) => {
     return (
-        <div>
-            <h2>Best One For You</h2>
+        <div className='button button1'>
+            <h4>Best One For You</h4>
             <h3>{fish.name}</h3>
 
         </div >
