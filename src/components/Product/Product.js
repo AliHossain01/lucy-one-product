621e5bd0e2defcb6e260be8b1352c569b764595f
AliHossain01 <alihossaincse01@gmail.com>
2022-03-26 18:47:38 +0600

    all product added from data.json file

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BsFillCartFill } from "react-icons/bs";
+import './Product.css';
 
-const Product = () => {
+const Product = ({ product, handleAddToCart }) => {
+    // const {product, handleAddToCart} = props;
+    const { id, name, price, image } = product;
+    //console.log(product);
+    //console.log(id);
+
     return (
-        <div>
+        <div className='product'>
+            <img src={image} alt=""></img>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
 
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <BsFillCartFill className='icon' />
+            </button>
         </div>
     );
 };
